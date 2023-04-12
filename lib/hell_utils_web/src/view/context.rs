6e88da06f17a0aa790a9112708b4e36b90a1d74e
helloki <helloki@mailbox.org>
2023-04-12 22:19:47 +0200

    feat (web): implement basic effect logic

--- lib/hell_utils_web/src/view/context.rs
+++ lib/hell_utils_web/src/view/context.rs
@@ -1,15 +1,19 @@
-use std::{cell::RefCell, any::Any, marker::PhantomData};
+use std::{cell::{RefCell, Cell}, any::Any, marker::PhantomData, collections::{HashMap, HashSet}, fmt::Display, borrow::BorrowMut};
 use crate::console_log;
 
 
-pub type InnerSignal = Box<dyn Any>;
+pub type InnerSignalValue = Box<dyn Any>;
+pub type InnerEffect      = Box<(dyn Fn() + 'static)>;
 
-#[derive(Debug)]
+// TODO (lm): look up slot-maps
 pub struct InnerRuntime {
     window: web_sys::Window,
     document: web_sys::Document,
 
-    signals: RefCell<Vec<InnerSignal>>
+    signal_values: RefCell<Vec<InnerSignalValue>>,
+    signal_subscribers: RefCell<HashMap<SignalId, HashSet<EffectId>>>,
+    effects: RefCell<Vec<InnerEffect>>,
+    running_effect: Cell<Option<EffectId>>,
 }
 
 impl InnerRuntime {
@@ -18,21 +22,33 @@ impl InnerRuntime {
         let document = window.document().expect("should have a document on window");
 
         let signals = RefCell::new(Vec::new());
+        let signal_subscribers = RefCell::new(HashMap::new());
+        let effects = RefCell::new(Vec::new());
+        let running_effect = Cell::new(None);
 
         Self {
             window,
             document,
 
-            signals,
+            signal_values: signals,
+            signal_subscribers,
+            effects,
+            running_effect,
         }
     }
 }
 
 // ----------------------------------------------------------------------------
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 pub struct Runtime {
     inner: &'static InnerRuntime,
+}
+
+impl std::fmt::Debug for Runtime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Runtime").finish()
+    }
 }
 
 impl Runtime {
@@ -41,42 +57,95 @@ impl Runtime {
         Self { inner }
     }
 
-    pub fn window(&self)       -> &web_sys::Window      { &self.inner.window }
-    pub fn document(&self)     -> &web_sys::Document    { &self.inner.document }
+    #[inline]
+    pub fn window(&self)       -> &web_sys::Window {
+        &self.inner.window
+    }
 
-    pub fn signals(&self) -> &RefCell<Vec<InnerSignal>> {
-        &self.inner.signals
+    #[inline]
+    pub fn document(&self) -> &web_sys::Document {
+        &self.inner.document
+    }
+
+    #[inline]
+    fn signal_values(&self) -> &RefCell<Vec<InnerSignalValue>> {
+        &self.inner.signal_values
+    }
+
+    #[inline]
+    fn effects(&self) -> &RefCell<Vec<InnerEffect>> {
+        &self.inner.effects
     }
 }
-
-// ----------------------------------------------------------------------------
 
 impl Runtime {
     pub fn create_signal<T>(&self, val: T) -> Signal<T>
     where T: Clone +'static
     {
-        console_log!("create signal");
+        console_log!("[SIGNAL]: start creating ...");
 
-        let mut signals = self.inner.signals.borrow_mut();
+        let mut signals = self.inner.signal_values.borrow_mut();
         signals.push(Box::new(val));
-        let id = SignalId(signals.len() - 1);
+        let signal_id = SignalId(signals.len() - 1);
+
+        console_log!("[SIGNAL]: created {}", signal_id.0);
 
         Signal {
             cx: *self,
-            id,
+            id: signal_id,
             _t: PhantomData,
         }
+    }
+
+    pub fn create_effect(&self, effect: impl Fn() + 'static) -> EffectId {
+        console_log!("[EFFECT]: start creating");
+
+        let effect_id = {
+            let mut effects = self.inner.effects.borrow_mut();
+            effects.push(Box::new(effect));
+            EffectId(effects.len() - 1)
+        };
+
+        self.run_effect(effect_id);
+
+        console_log!("[EFFECT]: created: '{}'", effect_id);
+
+        effect_id
+    }
+
+    fn run_effect(&self, effect_id: EffectId) {
+        let prev_effect = self.inner.running_effect.take();
+        self.inner.running_effect.set(Some(effect_id));
+
+        console_log!("[EFFECT] run: '{}'", effect_id);
+        let effect = &self.effects().borrow()[effect_id.0];
+        effect();
+
+        self.inner.running_effect.set(prev_effect);
     }
 }
 
 // ----------------------------------------------------------------------------
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct EffectId(usize);
-#[derive(Debug, Clone, Copy)]
+
+impl Display for EffectId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct SignalId(usize);
 
+impl Display for SignalId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
+// ----------------------------------------------
 
 #[derive(Debug, Clone, Copy)]
 pub struct Signal<T> {
@@ -89,20 +158,46 @@ impl<T> Signal<T> {
     pub fn get(&self) -> T
     where T: Clone + 'static
     {
-        self.cx.signals()
+        console_log!("[SIGNAL]: get: '{}'", self.id);
+
+        // retrieve value
+        let value = self.cx.signal_values()
             .borrow()[self.id.0]
             .downcast_ref::<T>()
             .unwrap()
-            .clone()
+            .clone();
+
+        // run effect initially
+        if let Some(running_effect_id) = self.cx.inner.running_effect.get() {
+            let mut subs = self.cx.inner.signal_subscribers.borrow_mut();
+            let subs = subs.entry(self.id).or_default();
+            let _newly_inserted = subs.insert(running_effect_id);
+            // assert!(newly_inserted);
+        }
+
+        value
     }
 
     pub fn set(&self, val: T)
     where T: 'static
     {
-        let wrapper = &mut self.cx.signals().borrow_mut()[self.id.0];
-        let wrapper = wrapper.downcast_mut::<T>().unwrap();
-        *wrapper= val;
+        console_log!("[SIGNAL]: set: '{}'", self.id);
+
+        {
+            let wrapper = &mut self.cx.signal_values().borrow_mut()[self.id.0];
+            let wrapper = wrapper.downcast_mut::<T>().unwrap();
+            *wrapper = val;
+        }
+
+        let subs = {
+            let subs = self.cx.inner.signal_subscribers.borrow();
+            subs.get(&self.id).cloned()
+        };
+
+        if let Some(subs) = subs {
+            for s in subs {
+                self.cx.run_effect(s);
+            }
+        }
     }
 }
-
-
